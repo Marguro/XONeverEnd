e888abb3fa6ede0faabe7e311f41786f3e34d5a2
Marguro <108455691+Marguro@users.noreply.github.com>
2025-01-14 13:11:18 +0700

    Change directory Assets Folder and create logic in game

---
+++ TicTacToe/GameResult.cs
@@ -0,0 +1,7 @@
+namespace TicTacToeWPF;
+
+public class GameResult
+{
+    public Player Winner { get; set; }
+    public WinInfo WinInfo { get; set; }
+}
---
+++ TicTacToe/GameState.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace TicTacToeWPF;
+
+public class GameState
+{
+    public Player [,] GameGrid { get; private set; }
+    public Player CurrentPlayer { get; private set; }
+    public int TurnPassed { get; private set; }
+    public bool GameOver { get; private set; }
+    
+    public event Action<int,int> MoveMade;
+    public event Action<GameResult> GameEnded;
+    public event Action GameRestarted;
+    
+    public GameState()
+    {
+        GameGrid = new Player[3,3];
+        CurrentPlayer = Player.X;
+        TurnPassed = 0;
+        GameOver = false;
+    }
+    
+    private bool CanMakeMove(int row, int column)
+    {
+        return GameGrid[row,column] == Player.None;
+    }
+    private bool IsGridFull()
+    {
+        return TurnPassed == 9;
+    }
+    
+    private void SwitchPlayer()
+    {
+        CurrentPlayer = CurrentPlayer == Player.X ? Player.O : Player.X;
+    }
+    
+    private bool AreSquaresMarked((int,int)[] squares, Player player)
+    {
+        foreach(var (row,column) in squares)
+        {
+            if(GameGrid[row,column] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    private bool DidMoveWin(int r,int c,out WinInfo winInfo)
+    {
+       (int,int)[] row = new []{(r,0),(r,1),(r,2)};
+       (int,int)[] col = new []{(0,c),(1,c),(2,c)};
+       (int,int)[] mainDiag = new []{(0,0),(1,1),(2,2)}; 
+       (int,int)[] antiDiag = new []{(0,2),(1,1),(2,0)};
+       
+         if(AreSquaresMarked(row,CurrentPlayer))
+         {
+              winInfo = new WinInfo{Type = WInType.Row,Number = r};
+              return true;
+         }
+         if (AreSquaresMarked(col,CurrentPlayer))
+         {
+              winInfo = new WinInfo{Type = WInType.Column,Number = c};
+              return true;
+         }
+         if (AreSquaresMarked(mainDiag,CurrentPlayer))
+         {
+             winInfo = new WinInfo{Type = WInType.Diagonal,Number = 0};
+             return true;
+         }
+         if (AreSquaresMarked(antiDiag,CurrentPlayer))
+         {
+             winInfo = new WinInfo{Type = WInType.ReverseDiagonal,Number = 0};
+             return true;
+         }
+         winInfo = null;
+        return false;
+    }
+    private bool DidMoveEndGame(int r,int c,out GameResult gameResult)
+    {
+        if(DidMoveWin(r,c,out WinInfo winInfo))
+        {
+            gameResult = new GameResult{Winner = CurrentPlayer,WinInfo = winInfo};
+            return true;
+        }
+        if(IsGridFull())
+        {
+            gameResult = new GameResult{Winner = Player.None};
+            return true;
+        }
+        gameResult = null;
+        return false;
+    }
+
+    public void MakeMove(int r, int c)
+    {
+        if (!CanMakeMove(r, c))
+        {
+            return;
+        }
+        GameGrid[r, c] = CurrentPlayer;
+        TurnPassed++;
+        
+        if(DidMoveEndGame(r,c,out GameResult gameResult))
+        {
+            GameOver = true;
+            MoveMade?.Invoke(r,c);
+            GameEnded?.Invoke(gameResult);
+        }
+        else
+        {
+            SwitchPlayer();
+            MoveMade?.Invoke(r,c);
+        }
+    }
+
+    public void Reset()
+    {
+        GameGrid = new Player[3,3];
+        CurrentPlayer = Player.X;
+        TurnPassed = 0;
+        GameOver = false;
+        GameRestarted?.Invoke();
+    }
+}
